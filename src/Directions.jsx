c2b86c52d90532d3878b6216b308894bf96b16d0
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-11-29 16:26:32 -0500

    2 files modified, 3 added, 2 deleted

---
+++ src/Directions.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+
+
+
+class Directions extends Component{
+  render(){
+    return(
+      <p>
+        <strong>
+          <em>
+            <li>
+              Type in the top box the current price and the bottom box the new box and it will calculate the result when you click calculate.
+            </li>
+          </em>
+        </strong>
+      </p>
+    )
+  }
+}
+
+
+export default Directions
---
+++ src/Input.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react"
+
+
+class Input extends Component{
+  initialState = {
+    current: 0 ,
+    newprice: 0,
+    percentage: 0,
+  }
+  state = this.initialState
+  render(){
+    return(
+      <section>
+
+
+<legend>New Price</legend>
+        <input type="number" onChange={this.handleChange}  value={this.state.newprice} name="newprice">
+        </input>
+
+
+        
+        <legend>Current Price</legend>
+        <input type="number" onChange={this.handleChange} value={this.state.current} name="current">
+        </input>
+
+        <input
+          type={"button"}
+          onClick={this.submitForm}
+          value={"Submit"}
+        ></input>
+
+        <h1>{this.state.percentage}</h1>
+      </section>
+    )
+  }
+
+  handleChange = (event) =>{
+    const {name,value} = event.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  submitForm = () =>{
+    this.setState({
+      percentage: this.doMath()
+    })
+  }
+
+  doMath = () =>{
+    return((this.state.newprice*100)/this.state.current)
+  }
+}
+
+export default Input
